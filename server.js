323071b39efc3ceae12037d2f9e86684cba79cb9
chamibia <b.chami39@gmail.com>
2021-12-22 12:43:50 -0500

    added another get

--- server.js
+++ server.js
@@ -42,12 +42,23 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
   results = filterByQuery(req.query, results);
   }
   res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
 
 app.listen(PORT, () => {
